ebfa9de543c019c65863d7e76586892cfef8a9b0
Oliver Tacke <o.tacke@posteo.de>
2024-04-29 15:53:46 +0200

    Use absolute path

--- .github/scripts/test.sh
+++ .github/scripts/test.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-. ./.github/scripts/configure-test.sh &&
-. ./.github/scripts/setup-github.sh &&
-. ./.github/scripts/setup-test.sh &&
-. ./.github/scripts/start-h5p-cli-server.sh &&
-. ./.github/scripts/run-playwright.sh &&
-. ./.github/scripts/stop-h5p-cli-server.sh
+SCRIPT_DIR=$(pwd)
+
+. "$SCRIPT_DIR/configure-test.sh" &&
+. "$SCRIPT_DIR/setup-github.sh" &&
+. "$SCRIPT_DIR/setup-test.sh" &&
+. "$SCRIPT_DIR/start-h5p-cli-server.sh" &&
+. "$SCRIPT_DIR/run-playwright.sh" &&
+. "$SCRIPT_DIR/stop-h5p-cli-server.sh"
