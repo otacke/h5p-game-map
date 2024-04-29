5145ed25d5da7378d42087cec07066f3605ac8ea
Oliver Tacke <o.tacke@posteo.de>
2024-04-29 21:32:32 +0200

    Add colors

--- .github/scripts/configure-test.sh
+++ .github/scripts/configure-test.sh
@@ -6,4 +6,7 @@ export H5P_CLI_TESTFILE_URL="https://h5p.org/sites/default/files/h5p/exports/ani
 export H5P_CLI_TESTCONTENT="testfile"
 export H5P_CLI_MACHINE_NAME=$(grep -E '"machineName"\s*:\s*"[^"]+"' library.json | grep -v ': {' | sed 's/.*"\([^"]*\)".*/\1/' | head -n 1)
 
-
+# Color palette
+export COLOR_OFF='\033[0m'
+export COLOR_RED='\033[0;31m'
+export COLOR_BLUE='\033[0;34m'
