59bfdee0c7b11e0e01da2976cda9fa1d65ae1b47
Oliver Tacke <o.tacke@posteo.de>
2026-06-02 13:29:53 +0200

    Ensure that H5P.Components.Button instance labels are visible first

--- src/scripts/components/overlay-dialogs/exercise-dialog.js
+++ src/scripts/components/overlay-dialogs/exercise-dialog.js
@@ -22,6 +22,10 @@ export default class ExerciseDialog extends OverlayDialog {
       this.instanceContainer.classList.toggle('blocked', params.isShowingSolutions);
     }
 
+    this.dom.querySelectorAll('.h5p-theme-button .h5p-theme-label').forEach((label) => {
+      label.style.display = 'block';
+    });
+
     super.show(params);
   }
 }
